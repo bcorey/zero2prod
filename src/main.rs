484e6ef53cb12e09bf4a671510b0efc429bd2eb0
Benjamin Corey <benjaminmcorey@gmail.com>
2023-03-02 17:43:45 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//! main.rs
+
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("failed to bind to port 8000");
+    run(listener)?.await
+}
